376e370841d92f12a4aa6887b4bb32755bbac458
AdrienPVM <adrien.pvm@gmail.com>
2017-11-10 10:40:39 +0100

    Root folders & root php creation

---
+++ public/contact.php
@@ -0,0 +1,13 @@
+<?php
+//J'inclus la config
+require __DIR__.'/../inc/config.php';
+
+
+//TODO Pleins de choses
+
+
+
+//A la fin, j'affiche
+require_once __DIR__.'/../view/header.php';
+require_once __DIR__.'/../view/contact.php';
+require_once __DIR__.'/../view/footer.php';
